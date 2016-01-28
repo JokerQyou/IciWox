851811b740d2061c64a87f5d966bd1714b4d027e
Joker Qyou <Joker.Qyou@gmail.com>
2016-01-28 19:07:51 +0800

    Open URL; Flake8 complaints

--- ici.py
+++ ici.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python
-#encoding:utf-8
+# encoding:utf-8
 from __future__ import unicode_literals
-
-import re
-from xml.dom import minidom
-from collections import namedtuple
 
 import requests
 import webbrowser
-from wox import Wox,WoxAPI
+from wox import Wox, WoxAPI
 
 KEY = 'E0F0D336AF47D3797C68372A869BDBC5'
 URL = 'http://dict-co.iciba.com/api/dictionary.php'
 
 
 class Main(Wox):
-
-    ex_count = 0
 
     def request(self, words):
         words = "_".join(words)
@@ -38,31 +32,29 @@ class Main(Wox):
                         part.get('part', '?'),
                         '\n\t'.join(part.get('means'))
                     ),
+                    # "IcoPath": "Images/app.ico",
+                    "JsonRPCAction": {
+                        # See https://git.io/vz74N
+                        "method": "open_url",
+                        "parameters": [
+                            'http://iciba.com/{}'.format(
+                                jsonobj.get('word_name')
+                            ),
+                        ],
+                        # 是否隐藏窗口
+                        "dontHideAfterAction": False,
+                    },
                 }
+
+    def open_url(self, url):
+        webbrowser.open(url)
+        WoxAPI.change_query(url)
 
     def query(self, args):
         response = self.request([i.lower() for i in args.split(' ')])
         if not response:
             return
         return [i for i in self.parse(response.json())]
-        for i in self.parse(root):
-            title = i[0]
-            url = None
-            results.append({
-                "Title": repr(title),
-                "SubTitle": i[-1].decode('utf-8'),
-                # "IcoPath": "Images/app.ico",
-                "JsonRPCAction": {
-                    # See https://github.com/qianlifeng/Wox/blob/master/Wox.Plugin/IPublicAPI.cs
-                    "method": "openUrl",
-                    #参数必须以数组的形式传过去
-                    "parameters": [url],
-                    #是否隐藏窗口
-                    "dontHideAfterAction": True
-                }
-            })
-
-        return results
 
 if __name__ == '__main__':
     Main()
